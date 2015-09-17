6d6c6e7ab1f138c4e76e33e2f3361e59d7247050
Sonia Martinez <soniamartinezctr@gmail.com>
2015-09-17 13:39:36 -0500

    pruebas

--- index.php
+++ index.php
@@ -4,4 +4,5 @@ require_once './constantes.php';
 require_once './autoload.php';
 $_SESSION[CONFIG] = parse_ini_file("config.ini", true);
 $controller = new SimpleFrontController();
-$controller->run();
+print 'index';
+//$controller->run();
