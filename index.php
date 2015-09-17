da83c0a8f5c5477ef012a7ac53318ac1d130b02e
Sonia Martinez <soniamartinezctr@gmail.com>
2015-09-17 14:24:25 -0500

    Se quito el comentario de htaccess

--- index.php
+++ index.php
@@ -4,5 +4,5 @@ require_once './constantes.php';
 require_once './autoload.php';
 $_SESSION[CONFIG] = parse_ini_file("config.ini", true);
 $controller = new SimpleFrontController();
-print 'index';
-//$controller->run();
+
+$controller->run();
