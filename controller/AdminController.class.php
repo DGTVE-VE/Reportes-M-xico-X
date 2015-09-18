b26e1e0146cd955232ed2277ba583c417d354511
Sonia Martinez <soniamartinezctr@gmail.com>
2015-09-18 12:57:58 -0500

    Prueba final de comprobación de correo inactivos

--- controller/AdminController.class.php
+++ controller/AdminController.class.php
@@ -50,31 +50,31 @@ class AdminController {
         $dao = DAOFactory::getAuthUserDAO();
         $resultInactivos = $dao->queryByIsActive($activo);
         foreach ($resultInactivos as $value) {
-        $correo = $value->email;
-        $hash = md5(date('Y-m-d H:i:s'));
-        $url = 'http://mx.televisioneducativa.gob.mx:81/ReportesMX/admin/activarUser/' . $correo . '/' . $hash;
-        $para = $correo;
-        $titulo = 'Recordatorio MéxicoX';
-        $mensaje = '<html><body>';
-        $mensaje .= '<h2>Recuerda activar tu cuenta de México X, para hacerlo da click en la siguiente dirección:</h2>';
-        $mensaje .= '<br>';
-        $mensaje .= '<h3>'.$url.'</h3>';
-        $mensaje .= '</body></html>';
-        $cabeceras = 'From: mexicox@televisioneducativa.gob.mx' . "\r\n" .
-                'Reply-To: mexicox@televisioneducativa.gob.mx' . "\r\n" .
-                "MIME-Version: 1.0\r\n". 
-                "Content-Type: text/html; charset=UTF-8\r\n".
-                'X-Mailer: PHP/' . phpversion();
+            $correo = $value->email;
+            $hash = md5(date('Y-m-d H:i:s'));
+            $url = 'http://mx.televisioneducativa.gob.mx:81/ReportesMX/admin/activarUser/' . $correo . '/' . $hash;
+            $para = $correo;
+            $titulo = 'Recordatorio de activación México X';
+            $mensaje = '<html><body>';
+            $mensaje .= '<h2>Recuerda activar tu cuenta de México X, ¡los cursos están por comenzar! para hacerlo da click en la siguiente dirección:</h2>';
+            $mensaje .= '<br>';
+            $mensaje .= '<h3>' . $url . '</h3>';
+            $mensaje .= '</body></html>';
+            $cabeceras = 'From: mexicox@televisioneducativa.gob.mx' . "\r\n" .
+                    'Reply-To: mexicox@televisioneducativa.gob.mx' . "\r\n" .
+                    "MIME-Version: 1.0\r\n" .
+                    "Content-Type: text/html; charset=UTF-8\r\n" .
+                    'X-Mailer: PHP/' . phpversion();
 
-        //     insertar registro en tabla Correo_activacion
-        $daoActiva = DAOFactory::getCorreoActivacionDAO();
-        $altaCorreoActivacion = new CorreoActivacion();
-        $altaCorreoActivacion->correo = $correo;
-        $altaCorreoActivacion->hash = $hash;
-        $daoActiva->insert($altaCorreoActivacion);
-
-        //enviar mail   
-        mail($para, $titulo, $mensaje, $cabeceras);
+            //     insertar registro en tabla Correo_activacion
+            $daoActiva = DAOFactory::getCorreoActivacionDAO();
+            $altaCorreoActivacion = new CorreoActivacion();
+            $altaCorreoActivacion->correo = $correo;
+            $altaCorreoActivacion->hash = $hash;
+            $daoActiva->insert($altaCorreoActivacion);
+//            print_r($url);
+            //enviar mail   
+            mail($para, $titulo, $mensaje, $cabeceras);
         }
         $_SESSION[VISTA] = 'view/reporteInscritosXCurso.php';
         include "templates/index.php";
@@ -83,8 +83,8 @@ class AdminController {
     public function activarUser($correo, $hash) {
         $daoActivado = DAOFactory::getCorreoActivacionDAO();
         $resultActivado = $daoActivado->queryByHashAndCorreo($correo, $hash);
-        if ($resultActivado != null) {          
-            
+        if ($resultActivado != null) {
+
 //        //update usuario activo
             $daoUsuario = DAOFactory::getAuthUserDAO();
             $consultaUsuario = $daoUsuario->queryByEmail($correo);
@@ -94,10 +94,9 @@ class AdminController {
             $daoActivado->deleteByCorreo($correo);
 //        mostrar vista de MX
             include "templates/usuarioActivado.php";
-        } else{
+        } else {
             header('Location: http://mx.televisioneducativa.gob.mx/');
         }
-        
     }
 
 }
--- view/recordatorio.php
+++ view/recordatorio.php
@@ -2,20 +2,5 @@
     <!--consultar Correos inactivos-->
     <input type="submit" onclick = "this.form.action = 'admin/consultarCorreosInactivos'" value="Consultar Correos Inactivos" />
     <br>
-    <?php 
-//    if (isset($totalInactivos)){
-//    $inactivos = unserialize(stripslashes($totalInactivos));   
-//    $a = 0;
-//    foreach ($inactivos as $value) {
-//        $correo = $inactivos[$a]->email;
-//        $hash = md5(date('Y-m-d H:i:s'));
-//        //$url = 'televisioneducativa.gob.mx:81/ReportesMX/admin/activarUser?m=' . $correo . 'h=' . $hash;
-//        $url = 'admin/activarUser/' . $correo . '/' . $hash;
-//        print '<a href="'.$url.'">'.$url.'</a>';
-//         print "<br>\n";        
-//        $a++;
-//    }}
-    ?>
-    <!--<input type="submit" onclick = "this.form.action = 'admin/enviarMail1'" value="Enviar mail" />-->
 </form>
 
