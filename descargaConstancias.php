8580617cd3940c7c269dd227aaa2b55e4e84e452
Israel Toledo <j.israel.toledo@gmail.com>
2015-11-25 11:09:37 -0600

    se le pusieron comillas al query

--- descargaConstancias.php
+++ descargaConstancias.php
@@ -36,7 +36,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT constancia FROM encuesta WHERE constancia = $ruta";
+$sql = "SELECT constancia FROM encuesta WHERE constancia = '$ruta'";
 $result = $conn->query($sql);
 
 if ($result->num_rows == 0) {
