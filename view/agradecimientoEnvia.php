4c48379134b9d9039540a2d579c1512677aa1b9f
Sonia Martinez <soniamartinezctr@gmail.com>
2015-12-16 13:10:49 -0600

    messaje

--- view/agradecimientoEnvia.php
+++ view/agradecimientoEnvia.php
@@ -49,9 +49,9 @@ function mail_attachment($file, $mailto, $from_mail, $from_name, $replyto, $subj
     $header .= "Content-Type: multipart/mixed; boundary=\"" . $uid . "\"\r\n\r\n";
     $header .= "This is a multi-part message in MIME format.\r\n";
     $header .= "--" . $uid . "\r\n";
-    $header .= "Content-Type: text/html; charset=UTF-8\r\n";
+    $header .= "Content-type:text/html; charset=iso-8859-1\r\n";
     $header .= "Content-Transfer-Encoding: 7bit\r\n\r\n";
-    $header .= $message . "\r\n\r\n";
+    //$header .= $message . "\r\n\r\n";
     $header .= "--" . $uid . "\r\n";
     $header .= "Content-Type: application/octet-stream; name=\"agradecimiento.pdf\"\r\n"; // use different content types here
     $header .= "Content-Transfer-Encoding: base64\r\n";
