36d4ac99483a8ac33de09e70242cee93f25d83d3
Sonia Martinez <soniamartinezctr@gmail.com>
2015-12-16 12:12:18 -0600

    commit 2

--- view/agradecimientoEnvia.php
+++ view/agradecimientoEnvia.php
@@ -34,7 +34,7 @@ foreach ($enviaAgradece as $value) {
 
 
 function mail_attachment($file, $mailto, $from_mail, $from_name, $replyto, $subject, $message) {
-//    $file = $filename;
+    $file = $filename;
     $file_size = filesize($file);
     $handle = fopen($file, "r");
 //    print 'file: '.$file.'<br>';
